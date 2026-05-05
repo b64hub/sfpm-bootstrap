2c3a72ef3dcc2eb77e8d5769bb73197524aba613
Nikolai Skaare <nikolai.skaare@b64.no>
2026-05-05 22:07:45 +0200

    chore: add lwcTypescriptHooks to sfpm configuration

--- sfpm.config.ts
+++ sfpm.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from '@b64/sfpm-core';
+import { lwcTypescriptHooks } from '@b64/sfpm-hooks'
 
 export default defineConfig({
-  sourceApiVersion: '65.0'
+  sourceApiVersion: '65.0',
+  hooks: [lwcTypescriptHooks()]
 });
